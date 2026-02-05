b920161eecff948d15d96ed55ddda4da7a9ae95a
Pritam Rao <pritam12.rao@gmail.com>
2026-02-06 00:14:16 +0530

    Setup Next.js + Elysia backend + React Query

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,10 @@
+import { Elysia, t } from "elysia";
+
+const rooms = new Elysia({prefix: '/room'}).post("/",()=> {
+    console.log("CREATE A NEW ROOM!")
+})
+
+const app = new Elysia({ prefix: "/api" }).use(rooms)
+
+export const GET = app.fetch;
+export const POST = app.fetch;
